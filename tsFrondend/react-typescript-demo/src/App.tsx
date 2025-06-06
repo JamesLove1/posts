684b9d532152222f27a225563f8a700ba4fa2a88
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-06-06 15:54:38 +0100

    updates

--- tsFrondend/react-typescript-demo/src/App.tsx
+++ tsFrondend/react-typescript-demo/src/App.tsx
@@ -2,24 +2,38 @@
 import './App.css';
 import Button from './components/Button';
 import Container from './components/Container';
+import Box from './components/context/Box';
+import ThemeContextProvidor from './components/context/ThemeContext';
+import { UserContextProvider } from './components/context/UserContext';
 import Greet from './components/Greet';
 import Heading from './components/Heading';
 import Input from './components/Input';
 import Oscar from './components/Oscar';
 import Person from './components/Person';
 import PersonList from './components/PersonList';
+import User from './components/state/User';
 import Status from './components/Status';
 
 function App() {
 
   return (
     <div className="App">
-      
+     
     </div>
   );
 }
 
 export default App;
+
+// Video 15 
+//  <UserContextProvider>
+//         <User />
+//       </UserContextProvider>
+
+// video 14 
+      // <ThemeContextProvidor>
+      //   <Box/>
+      // </ThemeContextProvidor>
 
 // video 7
      {/* <Container styles={{border: "1px solid black", padding: "1rem"}} /> */}
